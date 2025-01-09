721e3803d67a7cb99bb4a06b1160cf898f55de74
Ivan <ivangutsalenko92@gmail.com>
2025-01-09 17:14:57 +0200

    Add header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { DriversList } from './drivers/DriversList';
+import { Header } from './header/Header';
 
 export const App = () => {
     return (
         <div>
+            <Header />
             <DriversList />
         </div>
     );
